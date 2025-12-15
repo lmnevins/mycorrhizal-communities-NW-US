d680bc51d12078ddb14425161826baacdcd7e775
L. McKinley Nevins <lmnevins23@gmail.com>
2025-12-15 12:56:34 -0800

    feat: round one, then prep for funguild

--- 04_FungalTraits_Assignment.R
+++ 04_FungalTraits_Assignment.R
@@ -23,15 +23,16 @@ library(fungaltraits); packageVersion("fungaltraits")
 
 #################################################################################
 #                               Main workflow                                   #
-#    Use the FungalTraits database through R to assign guilds to ITS ASVs and   #
-#    then check how this compares to the previous assignments made by FungGuild #
+#  Use the FungalTraits database through R to assign guilds to ITS ASVs. For    #
+#  ASVs that do not have assignments, run through the FunGuild pipeline to see  #
+#  if we can get better coverage for assignments.                               #
 #                                                                               #
 #################################################################################
 
 
-###############
+################ -- 
 # (1) DATA PREP
-###############
+################ -- 
 
 wd <- "~/Dropbox/WSU/Mycorrhizae_Project/Community_Analyses/"
 setwd(wd)
@@ -69,14 +70,14 @@ ITS_otus$Genus <- sub("^g__", "", ITS_otus$Genus)
   
 # Count unique genera in the otu table 
 gen_count_otu <- ITS_otus %>%
-  count(Genus) %>%
+  dplyr::count(Genus) %>%
   filter(n > 1)
 # 401 genera, some appearing many times and others only once 
 # 6,827 have no genus assigned 
 
 # Count species assigned 
 spec_count_otu <- ITS_otus %>%
-  count(Species) %>%
+  dplyr::count(Species) %>%
   filter(n > 1)
 # 377 species, majority don't have species assignments, but for the ones that do I should match traits 
 # according to species for max accuracy 
@@ -101,7 +102,7 @@ traits2 <- dplyr::select(traits, species, speciesMatched, ifungorum_number, conf
 # Explore how many trait entries exist for each genus 
 
 gen_count_traits <- traits2 %>%
-  count(Genus) %>%
+  dplyr::count(Genus) %>%
   filter(n > 1)
 
 # 493 unique genera, ones with the most trait entries are Cladonia, Cortinarius, and Russula
@@ -119,90 +120,172 @@ Traits_unique <- traits2 %>%
 ### Data prep done - the Traits_unique dataframe has 3,591 unique Genus_Species IDs in the 'species' column, 
 # and the ITS_otus has the right columns for matching 
 
-###################################################################################################
+#################################################################################
+
+###################### -- 
+# (2) GUILD MATCHING 
+###################### -- 
 
 # Merge ITS_otus and traits2 to get the functional assignments
 # Many ASVs were not assigned to the genus level, so a big fraction won't get assignments here, but 
 # we have to go to the genus level to be accurate 
 
 
-# For accuracy, need to do a couple rounds here 
-
-# 1. First, match on full genus_species name to be most accurate, using 'species' column in both 
-otu_with_traits_species <- ITS_otus %>%
-  left_join(Traits_unique, by = "species")
-
-# guild_fg column has ectomycorrhizal assignment that I'm interested in 
-# Look at the different assignments by guild here 
-guild_count_species <- otu_with_traits_species %>%
-  count(guild_fg) %>%
-  filter(n > 1)
-# very very few with any usable assignment at this level 
-
-
-# 2.Try to match OTUs using just the Genus
+# 1. Try to match ASVs using just the Genus
 otu_with_traits_genus <- ITS_otus %>%
   left_join(Traits_unique, by = "Genus")
 
-# will have many-to-many matching because there are a lot of duplicate genera 
-
-# Look at the different assignments by guild here 
-guild_count_genus <- otu_with_traits_genus %>%
-  count(guild_fg) %>%
-  filter(n > 1)
-
-# Gotta be doing it at the genus level! 
+# NOTE: many-to-many join is expected at the genus level
 
 
-# take genus assignments dataframe and check for variation in assignments within a genus 
-
-# sort to only include those with guild_fg assignments 
-
+# 2. Keep only rows with guild assignments
 otu_with_traits_genus$guild_fg <- as.factor(otu_with_traits_genus$guild_fg)
 
 traits_genus_trim <- otu_with_traits_genus %>%
   filter(!is.na(guild_fg))
-# this gives 10,896 rows that have genus and guild assignments 
+# ~10,896 rows with genus-level guild assignments
 
 
-# Now want to sort down to just guilds that are relevant to ectomycorrhizae
+# 3. Restrict to ectomycorrhizal-related guilds
 # There are 6 classifications that include 'Ectomycorrhizal' in the name 
-
 traits_genus_ecto <- traits_genus_trim %>%
-  filter(guild_fg %in% c("Ectomycorrhizal", "Ectomycorrhizal-Endophyte-Ericoid Mycorrhizal-Litter Saprotroph-Orchid Mycorrhizal",
-                         "Ectomycorrhizal-Fungal Parasite", "Ectomycorrhizal-Orchid Mycorrhizal-Root Associated Biotroph",
-                         "Ectomycorrhizal-Undefined Saprotroph", "Ectomycorrhizal-Wood Saprotroph"))
-# 5,432 rows, still with some duplication 
+  filter(guild_fg %in% c(
+    "Ectomycorrhizal",
+    "Ectomycorrhizal-Endophyte-Ericoid Mycorrhizal-Litter Saprotroph-Orchid Mycorrhizal",
+    "Ectomycorrhizal-Fungal Parasite",
+    "Ectomycorrhizal-Orchid Mycorrhizal-Root Associated Biotroph",
+    "Ectomycorrhizal-Undefined Saprotroph",
+    "Ectomycorrhizal-Wood Saprotroph"
+  ))
+# ~5,432 rows (still duplicated by OTU)
 
 
-# Find currently duplicated OTUs to double check before filtering to just unique 
+# 4. An OTU is considered valid if it has AT LEAST ONE
+# non-Possible ectomycorrhizal assignment
 
-dupe_otus <- traits_genus_ecto %>%
-  add_count(OTU, name = "otu_count") %>%
-  filter(otu_count > 1)
-# At most an OTU is duplicated 8 times, at least twice (obvs)
+otu_validity <- traits_genus_ecto %>%
+  group_by(OTU) %>%
+  summarise(
+    has_valid_assignment = any(confidence_fg != "Possible"),
+    .groups = "drop"
+  )
 
-# There is some variation for the species coming from the FungalTraits dataset, but there is no variation in 
-# the guild_fg assignment at the genus level, so there should be no problem with trimming duplicate OTUs. 
+valid_otus <- otu_validity %>%
+  filter(has_valid_assignment) %>%
+  pull(OTU)
 
-# Keep distinct OTUs for the final dataset 
 
-final_otu_traits <- traits_genus_ecto %>%
+# 5. Keep only valid OTUs, then drop low-confidence rows
+traits_genus_ecto_valid <- traits_genus_ecto %>%
+  filter(OTU %in% valid_otus) %>%
+  filter(confidence_fg != "Possible")
+
+
+# 6. collapse to one row per OTU
+final_otu_traits <- traits_genus_ecto_valid %>%
   distinct(OTU, .keep_all = TRUE)
 
-# 2,140 retained 
+# ~2,140 retained
 
-# Check it out 
 
+# Checks
 final_otu_traits$Genus <- as.factor(final_otu_traits$Genus)
+final_otu_traits$confidence_fg <- as.factor(final_otu_traits$confidence_fg)
+
+summary(final_otu_traits$confidence_fg)
+
+
+# No low-confidence assignments retained
+final_otu_traits %>%
+  filter(confidence_fg == "Possible") %>%
+  nrow()
+
+# No NA guilds
+final_otu_traits %>%
+  filter(is.na(guild_fg)) %>%
+  nrow()
 
 
 # Save dataset for trait assignments from the literature 
 write.csv(final_otu_traits, "~/Dropbox/WSU/Mycorrhizae_Project/Community_Analyses/Functional_analyses/EM_FungalTraits_final.csv")
 
 
-## Conclusion was that while more ASV's were given assignments, 15 fewer genera had assignments than what 
-# was done using FunGuild, so the coverage was much poorer. 
+## Conclusion: 2,053 ASVs were given guild assignments to be ectomycorrhizal. Can subset the ones that have been ID'ed 
+# to the genus level but did not have functional guild assignments made, and run these through FunGuild to see if we 
+# can get more coverage. 
 
-# This is bring retained as a comparison, but the original FunGuild assignments were used for all downsteam steps 
 
+#################################################################################
+
+########################### -- 
+# (3) SUBSET FOR FUNGUILD 
+########################### -- 
+
+# Subset ones with genus info but no assignment, or which had 'possible' as the 
+# confidence level so they can be checked with FUNGuild instead.
+
+# In the otu_with_traits_genus dataframe, only ASVs with an assignment are given a confidence_fg rank, 
+# and ones with no rank just have 'NA'. Therefore, this can be filtered to select ASVs with either 
+# 'Possible' or 'NA' ranks to be processed through FUNGuild 
+
+# Use 'otu_with_traits_genus' df because this one shows which ASVs have genus info, and then 
+# can be filtered for which don't have guild assignments 
+
+# Check assignment status for all ASVs
+otu_assignment_status <- otu_with_traits_genus %>%
+  group_by(OTU) %>%
+  summarise(
+    has_high_confidence = any(confidence_fg %in% c("Probable", "Highly Probable")),
+    .groups = "drop"
+  ) %>%
+  mutate(
+    to_reprocess = !has_high_confidence
+  )
+
+summary(otu_assignment_status)
+
+
+# find ASVs that need reprocessing
+reprocess_otus <- otu_assignment_status %>%
+  filter(to_reprocess) %>%
+  pull(OTU)
+
+
+# create final no guild dataset 
+final_no_guild <- otu_with_traits_genus %>%
+  filter(OTU %in% reprocess_otus) %>%
+  distinct(OTU, .keep_all = TRUE)
+
+# Checks 
+
+# No high-confidence assignments in reprocess set
+otu_with_traits_genus %>%
+  filter(OTU %in% final_no_guild$OTU,
+         confidence_fg %in% c("Probable", "Highly Probable")) %>%
+  nrow()
+
+# No overlap with assigned set
+intersect(final_no_guild$OTU, final_otu_traits$OTU)
+
+# 13,458 ASVs
+
+
+## Needs to be in particular format for FUNGuild, can find info about it here: 
+# https://github.com/UMNFuN/FUNGuild
+
+# The df already has the taxonomy column FUNGuild needs, and has OTUs in rows and the samples in columns 
+
+# Just need to remove the excess columns and save 
+
+final_no_guild <- dplyr::select(final_no_guild, -c(Kingdom, Phylum, Class, Order, Family, 
+                                                               Genus, Species, species.x, species.y, speciesMatched,
+                                                               ifungorum_number, confidence_fg, growth_form_fg, guild_fg,
+                                                               higher_clade, substrate, taxonomic_level_fg, trait_fg, 
+                                                               trophic_mode_fg))
+#save dataframe as a csv file 
+write.csv(final_no_guild, "./FUNGuild/ITS_OTU_table_2025.csv")
+
+#saves with rownumbers in the first column. After it saves, open it up in Excel just 
+#to doublecheck it, and delete that first column. 
+
+## -- END -- ## 
